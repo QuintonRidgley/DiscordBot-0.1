aedf42b4684d5e8b668886cb2893d3063952432c
Quinton <57518368+QuintonRidgley@users.noreply.github.com>
2019-11-12 09:31:55 -0600

    Update Baseline.py

--- Baseline.py
+++ Baseline.py
@@ -1,13 +1,13 @@
 #working baseline
-# still need a command function
+#all this does is prints into the terminal saying that the Bot has a successful link
 
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '!')
+client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
 	print('Bot is ready.')
 
-client.run('MyToken')
+client.run('MY_TOKEN')
